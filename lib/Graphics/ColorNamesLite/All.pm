82cb024b68d5f0d9af18e5a0042ce7b9a9d28605
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-30 15:32:20 +0700

    Add PantoneReport

--- lib/Graphics/ColorNamesLite/All.pm
+++ lib/Graphics/ColorNamesLite/All.pm
@@ -1,12 +1,14 @@
 package Graphics::ColorNamesLite::All;
 
+use strict;
+
 # AUTHORITY
 # DATE
 # DIST
 # VERSION
 
-# note: SVG=CSS, WWW=CSS. GrayScale returns coderef.
-# CODE: no strict 'refs'; use Data::Dump; my $t = {}; for my $m (qw/CSS Crayola EmergyC HTML IE Mozilla Netscape VACCC Werner Windows X HTML_ID /) { my $mod = "Graphics::ColorNames::$m"; (my $modpm = "$mod.pm") =~ s!::!/!g; require $modpm; my $t2 = &{"$mod\::NamesRgbTable"}(); die "$m" unless ref $t2 eq "HASH"; for (keys %$t2) { $t->{$_} = sprintf "%06x", $t2->{$_} unless defined $t->{$_} } } print 'our $NAMES_RGB_TABLE = '; dd $t; print ";";
+# note: SVG=CSS, WWW=CSS. GrayScale returns coderef. Pantone are numbers.
+# CODE: no strict 'refs'; use Data::Dump; my $t = {}; for my $m (qw/CSS Crayola EmergyC HTML IE Mozilla Netscape PantoneReport VACCC Werner Windows X HTML_ID /) { my $mod = "Graphics::ColorNames::$m"; (my $modpm = "$mod.pm") =~ s!::!/!g; require $modpm; my $t2 = &{"$mod\::NamesRgbTable"}(); die "$m" unless ref $t2 eq "HASH"; for (keys %$t2) { $t->{$_} = sprintf "%06x", $t2->{$_} unless defined $t->{$_} } } print 'our $NAMES_RGB_TABLE = '; dd $t; print ";";
 
 1;
 # ABSTRACT: Lots of color names and equivalent RGB values (lite version)
